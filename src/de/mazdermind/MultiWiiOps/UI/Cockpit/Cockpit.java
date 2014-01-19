403c05068c0f450fb2d89b724a61df0b86d1ed67
MaZderMind <github@mazdermind.de>
2014-01-19 23:50:56 +0100

    experiment with larger insets - looks good

--- src/de/mazdermind/MultiWiiOps/UI/Cockpit/Cockpit.java
+++ src/de/mazdermind/MultiWiiOps/UI/Cockpit/Cockpit.java
@@ -17,19 +17,19 @@ public class Cockpit extends JPanel {
 
 	public Cockpit() {
 		setLayout(new GridBagLayout());
-		
+
 		bearingPanel = new BearingPanel();
-		add(bearingPanel, 
+		add(bearingPanel,
 			new GBC(0, 0)
 			.setInsets(5)
 			.setSpan(3, 1)
 			.setWeight(2, 3)
 			.setFill(GBC.BOTH));
-		
+
 		statusPanel = new StatusPanel();
-		add(statusPanel, 
+		add(statusPanel,
 			new GBC(0, 1)
-			.setInsets(5)
+			.setInsets(20, 5, 20, 5)
 			.setSpan(3, 1)
 			.setWeight(2, 1.5)
 			.setFill(GBC.BOTH));
@@ -47,12 +47,11 @@ public class Cockpit extends JPanel {
 				.setInsets(5)
 				.setWeight(1.3, 0)
 				.setFill(GBC.HORIZONTAL));
-		
 
 		TestPanel gpsPanel = new TestPanel("gps");
 		add(gpsPanel,
 				new GBC(3, 0)
-				.setInsets(5)
+				.setInsets(5, 20, 5, 5)
 				.setSpan(1, 3)
 				.setWeight(2, 1)
 				.setFill(GBC.BOTH));
