f070ec4ae6dd90f04991e88454033bf981c9536f
MaZderMind <github@mazdermind.de>
2014-01-19 23:32:42 +0100

    implement nice and disable-able ;) status leds

--- src/de/mazdermind/MultiWiiOps/UI/Cockpit/StatusLedPanel.java
+++ src/de/mazdermind/MultiWiiOps/UI/Cockpit/StatusLedPanel.java
@@ -2,16 +2,24 @@ package de.mazdermind.MultiWiiOps.UI.Cockpit;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GradientPaint;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.RoundRectangle2D;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class StatusLedPanel extends JPanel {
+import com.sun.corba.se.spi.legacy.connection.GetEndPointInfoAgainException;
+
+public class StatusLedPanel extends JLabel {
 	private static final long serialVersionUID = 7663175559342904288L;
 
-	public StatusLedPanel(String name, Color color) {
+	public StatusLedPanel(String text, Color color) {
 		setBackground(color);
-		//setEnabled(false);
+		setText(text);
 	}
 
 	@Override
@@ -21,6 +29,28 @@ public class StatusLedPanel extends JPanel {
 	
 
 	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
+		Graphics2D g2d = (Graphics2D)g;
+		String text = getText();
+		Color color = getBackground();
+
+		Rectangle2D stringBounds = g2d.getFontMetrics().getStringBounds(text, g2d);
+
+		int
+			w = getWidth(),
+			h = getHeight(),
+			tx = (int)((w - stringBounds.getWidth()) / 2),
+			ty = (int)(w/5);
+		
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		
+		g2d.setColor(isEnabled() ? Color.BLACK : Color.DARK_GRAY);
+		g2d.fillRoundRect(0, 0, w, h, 10, 10);
+		
+		g2d.setColor(isEnabled() ? color : color.darker().darker());
+		g2d.fillRoundRect(20, (int)(w/3.3), w-35, (int)(h/3.5), 3, 3);
+		
+		g2d.setColor(isEnabled() ? Color.WHITE : Color.LIGHT_GRAY);
+		g2d.drawString(getText(), tx, ty);
 	}
 }
--- src/de/mazdermind/MultiWiiOps/UI/Cockpit/StatusPanel.java
+++ src/de/mazdermind/MultiWiiOps/UI/Cockpit/StatusPanel.java
@@ -7,26 +7,43 @@ import javax.swing.JLabel;
 
 public class StatusPanel extends JLabel {
 	private static final long serialVersionUID = 5999631872010288870L;
+	private StatusLedPanel acc, baro, mag, arm, stable, horizon, gps, sonar, optic, althold, headfree, headadj;
 
 	public StatusPanel() {
 		setLayout(new GridLayout(2, 6, 20, 20));
 		setOpaque(false);
-
-		add(new StatusLedPanel("ACC", Color.GREEN));
-		add(new StatusLedPanel("BARO", Color.GREEN));
-		add(new StatusLedPanel("MAG", Color.GREEN));
 		
-		add(new StatusLedPanel("ARM", Color.YELLOW));
-		add(new StatusLedPanel("STABLE", Color.YELLOW));
-		add(new StatusLedPanel("HORIZON", Color.YELLOW));
+		acc = new StatusLedPanel("ACC", Color.GREEN);
+		acc.setEnabled(false);
+		add(acc);
+		baro = new StatusLedPanel("BARO", Color.GREEN);
+		baro.setEnabled(false);
+		add(baro);
+		mag = new StatusLedPanel("MAG", Color.GREEN);
+		add(mag);
 		
-		add(new StatusLedPanel("GPS", Color.GREEN));
-		add(new StatusLedPanel("SONAR", Color.GREEN));
-		add(new StatusLedPanel("OPTIC", Color.GREEN));
+		arm = new StatusLedPanel("ARM", Color.YELLOW);
+		add(arm);
+		stable = new StatusLedPanel("STABLE", Color.YELLOW);
+		add(stable);
+		stable.setEnabled(false);
+		horizon = new StatusLedPanel("HORIZON", Color.YELLOW);
+		add(horizon);
 		
-		add(new StatusLedPanel("ALTHOLD", Color.YELLOW));
-		add(new StatusLedPanel("HEADFREE", Color.YELLOW));
-		add(new StatusLedPanel("HEADADJ", Color.YELLOW));
+		gps = new StatusLedPanel("GPS", Color.GREEN);
+		add(gps);
+		sonar = new StatusLedPanel("SONAR", Color.GREEN);
+		add(sonar);
+		optic = new StatusLedPanel("OPTIC", Color.GREEN);
+		add(optic);
+		
+		althold = new StatusLedPanel("ALTHOLD", Color.YELLOW);
+		althold .setEnabled(false);
+		add(althold);
+		headfree = new StatusLedPanel("HEADFREE", Color.YELLOW);
+		add(headfree);
+		headadj = new StatusLedPanel("HEADADJ", Color.YELLOW);
+		add(headadj);
 	}
 
 }
