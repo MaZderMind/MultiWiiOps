3366cd9a66c386026ef54b56870d55ce82eace42
MaZderMind <github@mazdermind.de>
2014-01-19 14:08:46 +0100

    missing round/square classes

---
+++ src/de/mazdermind/MultiWiiOps/UI/Cockpit/BearingPanel.java
@@ -0,0 +1,19 @@
+package de.mazdermind.MultiWiiOps.UI.Cockpit;
+
+import java.awt.GridLayout;
+
+import javax.swing.JPanel;
+
+import de.mazdermind.MultiWiiOps.UI.RoundTestPanel;
+
+public class BearingPanel extends JPanel {
+	private static final long serialVersionUID = -7197088755218969321L;
+
+	public BearingPanel() {
+		setLayout(new GridLayout(1, 2, 2*5, 0));
+		setOpaque(false);
+		
+		add(new RoundTestPanel("horizon"));
+		add(new RoundTestPanel("compass"));
+	}
+}
---
+++ src/de/mazdermind/MultiWiiOps/UI/RoundTestPanel.java
@@ -0,0 +1,45 @@
+package de.mazdermind.MultiWiiOps.UI;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.geom.Rectangle2D;
+
+public class RoundTestPanel extends TestPanel {
+	private static final long serialVersionUID = 4813133879294967482L;
+
+	public RoundTestPanel() {
+		super();
+	}
+
+	public RoundTestPanel(Color bg) {
+		super(bg);
+	}
+
+	public RoundTestPanel(String label, Color bg) {
+		super(label, bg);
+	}
+
+	public RoundTestPanel(String label) {
+		super(label);
+	}
+
+	public void paintComponent(Graphics g) {
+		Rectangle2D stringBounds = g.getFontMetrics().getStringBounds(getText(), g);
+		int
+			w = getWidth() - 1,
+			h = getHeight() - 1,
+			sz = Math.min(w,  h),
+			x = (w - sz) / 2,
+			y = (h - sz) / 2,
+			tx = (int) ((w - stringBounds.getWidth()) / 2),
+			ty = (int) ((h + stringBounds.getHeight()) / 2);
+
+		Color bg = getBackground();
+		g.setColor(bg);
+		g.fillArc(x, y, sz, sz, 0, 360);
+		
+		g.setColor(getForeground());
+		g.drawString(getText(), tx, ty);
+		
+   }
+}
