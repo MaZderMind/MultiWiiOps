afdfbbfa76130e95caeb60e6e5d70cdb115b3bfe
Peter <github@mazdermind.de>
2014-02-03 17:40:38 +0100

    fix & document rendering

--- src/de/mazdermind/MultiWiiOps/UI/Cockpit/HorizonPanel.java
+++ src/de/mazdermind/MultiWiiOps/UI/Cockpit/HorizonPanel.java
@@ -6,6 +6,7 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GraphicsConfiguration;
+import java.awt.Image;
 import java.awt.Polygon;
 import java.awt.RenderingHints;
 import java.awt.Transparency;
@@ -19,11 +20,26 @@ import java.util.logging.Logger;
 import javax.swing.JPanel;
 
 public class HorizonPanel extends JPanel {
-	private static final Logger log = Logger.getLogger( HorizonPanel.class.getName() );
 	private static final long serialVersionUID = -8537524917689154790L;
+	private static final Logger log = Logger.getLogger( HorizonPanel.class.getName() );
+
+	/**
+	 * default air color (light blue)
+	 */
 	public static final Color AIR = new Color(183, 225, 236);
+
+	/**
+	 * default earth color (light brown)
+	 */
 	public static final Color EARTH = new Color(118, 86, 57);
-	private static final int YSCALE = 4;
+
+	/**
+	 * PITCH_SCALE=1 would display 360° (-180° to +180°) from top to bottom of the artificial horizon
+	 * PITCH_SCALE=4 displays 90° (-45° to 45°).
+	 *
+	 * The Pitch-Arrow to the right always shows the full 360° scale
+	 */
+	private static final int PITCH_SCALE = 4;
 
 	private float roll = 180, pitch = 180;
 	private Polygon triangle;
@@ -43,10 +59,12 @@ public class HorizonPanel extends JPanel {
 		setForeground(air);
 		setOpaque(false);
 
+
 		int xPoly[] = {0, 5, 10};
 		int yPoly[] = {0, 10, 0};
 		triangle = new Polygon(xPoly, yPoly, xPoly.length);
 
+		// experimental mouse input routines to test pitch- & roll display
 		final HorizonPanel horizonPanel = this;
 		addMouseListener(new MouseAdapter() {
 			@Override
@@ -76,21 +94,31 @@ public class HorizonPanel extends JPanel {
 	}
 
 	/**
-	 * custom paint routine
+	 * Paint all Layers that constitutes the artificial Horizon
 	 */
 	public void paintComponent(Graphics g) {
 		// g is actually a g2; it just doesn't know yet ;)
 		Graphics2D g2 = (Graphics2D)g;
 
-		drawHorizon(g2);
-		drawArrows(g2);
+		// Device-Configuration used to generate the image Layer
+		GraphicsConfiguration dc = g2.getDeviceConfiguration();
+
+		// generate layers and merge them onto the canvas
+		g2.drawImage(generateHorizonLayer(dc), 0, 0, null);
+		g2.drawImage(generateTextLayer(dc), 0, 0, null);
+		g2.drawImage(generateArrowLayer(dc), 0, 0, null);
 	}
 
+
+
+
+
 	/**
-	 * draws the roll and pitch indicators at the bottom and right side
-	 * @param g2 Graphics-Context to operate on
+	 * Generate an Image-Layer containing the Earth- and the Water-Area, cropped to a circle
+	 * @param dc Graphics-Device-Configuration the returned Image should be compatible to
+	 * @return Image-Layer with Earth- and the Water-Area
 	 */
-	private void drawArrows(Graphics2D g2) {
+	private Image generateHorizonLayer(GraphicsConfiguration dc) {
 		int
 			w = getWidth() - 1,
 			h = getHeight() - 1,
@@ -98,127 +126,152 @@ public class HorizonPanel extends JPanel {
 			x = (w - sz) / 2,
 			y = (h - sz) / 2;
 
-		g2.setColor(Color.BLACK);
-		g2.drawLine(x, y+sz, x+sz, y+sz);
-		g2.drawLine(x+sz, y, x+sz, y+sz);
-
-		AffineTransform oldTransform = g2.getTransform();
-
-		g2.translate(x - 5 + ((1 - roll / 360) * sz), y+sz-10);
-		g2.fillPolygon(triangle);
-
-		g2.setTransform(oldTransform);
-
-		int myx = x + sz - 10;
-		int myy = (int) (y + 5 + ((pitch / 360) * sz));
-		g2.rotate(Math.toRadians(-90), myx, myy);
-		g2.translate(myx, myy);
-		g2.fillPolygon(triangle);
-	}
-
-	/**
-	 * draws the virtual horizon as rotated and clipped areas
-	 * TODO: also draw textual and center markers
-	 * @param g2 Graphics-Context to operate on
-	 */
-	private void drawHorizon(Graphics2D g2) {
-		int
-			w = getWidth() - 1,
-			h = getHeight() - 1,
-			sz = Math.min(w,  h) - 3,
-			x = (w - sz) / 2,
-			y = (h - sz) / 2;
-
-		// code borrowed from https://weblogs.java.net/blog/campbell/archive/2006/07/java_2d_tricker.html
-		// thank you, Chris Campbell!
-
-		// Create a translucent intermediate image in which we can perform
-		// the soft clipping
-		GraphicsConfiguration gc = g2.getDeviceConfiguration();
-		BufferedImage img = gc.createCompatibleImage(w, h, Transparency.TRANSLUCENT);
+		BufferedImage img = dc.createCompatibleImage(w, h, Transparency.TRANSLUCENT);
 		Graphics2D gfx = img.createGraphics();
 
-		// Clear the image so all pixels have zero alpha
-		gfx.setComposite(AlphaComposite.Clear);
-		gfx.fillRect(0, 0, w, h);
-
-		// Render our clip shape into the image.  Note that we enable
-		// anti-aliasing to achieve the soft clipping effect.  Try
-		// commenting out the line that enables anti-aliasing, and
-		// you will see that you end up with the usual hard clipping.
 		gfx.setComposite(AlphaComposite.Src);
 		gfx.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		gfx.setColor(Color.WHITE);
 		gfx.fillOval(x, y, sz, sz);
-
-		// Here's the trick... We use SrcAtop, which effectively uses the
-		// alpha value as a coverage value for each pixel stored in the
-		// destination.  For the areas outside our clip shape, the destination
-		// alpha will be zero, so nothing is rendered in those areas.  For
-		// the areas inside our clip shape, the destination alpha will be fully
-		// opaque, so the full color is rendered.  At the edges, the original
-		// anti-aliasing is carried over to give us the desired soft clipping
-		// effect.
 		gfx.setComposite(AlphaComposite.SrcAtop);
+
 		gfx.rotate(Math.toRadians(180 - roll), w/2, h/2);
 
-		// paint content
-		drawHorizonContent(x, w, h, sz, gfx);
-
-		gfx.dispose();
-
-		// Copy our intermediate image to the screen
-		g2.drawImage(img, 0, 0, null);
-	}
-
-	private void drawHorizonContent(int x, int w, int h, int sz, Graphics2D gfx) {
 		gfx.setColor(getBackground());
 		gfx.fillRect(0, 0, w, h);
 
 		gfx.setColor(getForeground());
-		gfx.fillRect(0, 0, w, (int)(h/2 + ((pitch-180) / 360 * sz * YSCALE)));
+		gfx.fillRect(0, 0, w, (int)(h/2 + ((pitch-180) / 360 * sz * PITCH_SCALE)));
 
-		GraphicsConfiguration gc = gfx.getDeviceConfiguration();
-		BufferedImage imgText = gc.createCompatibleImage(w, h, Transparency.TRANSLUCENT);
-		Graphics2D gfxText = imgText.createGraphics();
+		gfx.dispose();
 
-		// anti alias is working when painting on gfx but not on gfxText
-		gfxText.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		gfxText.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		return img;
+	}
 
-		float h360 = (float)sz/360*YSCALE;
-		int w2 = sz/2, h2 = sz/2;
-		int w4 = sz/4, w43 = w4*3;
-		int w3 = sz/3, w32 = w3*2;
+	/**
+	 * Generate an Image-Layer with the Text inside the Horizon (Degree-Numbers and Lines)
+	 * @param dc Graphics-Device-Configuration the returned Image should be compatible to
+	 * @return Image-Layer with the Horizon-Text
+	 */
+	private Image generateTextLayer(GraphicsConfiguration dc) {
+		// Useful geometric calculations
+		int
+			w = getWidth() - 1,    // usable width
+			h = getHeight() - 1,   // usable height
+			sz = Math.min(w,  h), // center-circle-size
+			sz2 = sz/2,            // half the size of the center-circle
+			x = (w - sz) / 2,      // horizontal center
+			w4 = sz/4, w43 = w4*3, // 1/4 and 3/4 as well as
+			w3 = sz/3, w32 = w3*2; // 1/3 and 2/3 of the center circle width
 
+		float h360 = (float)sz/360*PITCH_SCALE;
+
+		// Initialize Layer-Image
+		BufferedImage img = dc.createCompatibleImage(w, h, Transparency.TRANSLUCENT);
+		Graphics2D gfx = img.createGraphics();
+
+		gfx.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		gfx.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+
+		// Rotate canvas to matching roll-axis
+		gfx.rotate(Math.toRadians(180 - roll), w/2, h/2);
+
+		// draw center-Line
 		gfx.setColor(Color.WHITE);
-		gfxText.setColor(Color.WHITE);
+		gfx.drawLine(x+w3, h/2, x+w32, h/2);
 
-		gfxText.drawLine(x+w3, h/2, x+w32, h/2);
-
+		// draw names and lines fot the following angles
 		int angles[] = {150,160,170,190,200,210};
+
+		final int extraTextSpacingX = 5;
+
 		for (int i = 0; i < angles.length; i++) {
 			int angle = angles[i];
-			int y = (int)(h360 * (angle - 180)) + h2;
 
+			// y-location of the line
+			int txty = (int)(h360 * (angle - 180)) + sz2;
+
+			// first and last line is drawn shorter (1/3 of the available width)
 			if(i == 0 || i == angles.length-1)
-				gfxText.drawLine(w3+x, y, w32+x, y);
+				gfx.drawLine(w3+x, txty, w32+x, txty);
+
+			// other lines are drawn longer (3/4 of the width)
 			else
-				gfxText.drawLine(w4+x, y, w43+x, y);
+				gfx.drawLine(w4+x, txty, w43+x, txty);
 
-			String angleStr = String.valueOf(angle);
-			Rectangle2D stringBounds = gfxText.getFontMetrics().getStringBounds(angleStr, gfx);
+			// angular string
+			String angleStr = Integer.toString(angle);
+			Rectangle2D stringBounds = gfx.getFontMetrics().getStringBounds(angleStr, gfx);
 
-			gfxText.setComposite(AlphaComposite.Clear);
-			gfxText.fillRect(x + w2 - (int)stringBounds.getCenterX() - 5, y - 4 + (int)stringBounds.getCenterY(), (int)stringBounds.getWidth() + 10, (int)stringBounds.getHeight());
+			// clear the area required by the string from the line below it
+			gfx.setComposite(AlphaComposite.Clear);
+			gfx.fillRect(x + sz2 - (int)stringBounds.getCenterX() - extraTextSpacingX, txty + (int)stringBounds.getCenterY(), (int)stringBounds.getWidth() + 2*extraTextSpacingX, (int)stringBounds.getHeight());
 
-			gfxText.setComposite(AlphaComposite.Src);
-			gfxText.drawString(angleStr, x + w2 - (int)stringBounds.getCenterX(), y - (int)stringBounds.getCenterY());
+			// draw the text into the cleared area
+			gfx.setComposite(AlphaComposite.Src);
+			gfx.drawString(angleStr, x + sz2 - (int)stringBounds.getCenterX(), txty - (int)stringBounds.getCenterY());
 		}
 
-		gfxText.dispose();
-		gfx.drawImage(imgText, 0, 0, null);
+		// return the Layer-Image
+		gfx.dispose();
+		return img;
 	}
+
+	/**
+	 * Generate an Image-Layer with the Roll- & Pitch-Lines and the respective Arrows on it
+	 * @param dc Graphics-Device-Configuration the returned Image should be compatible to
+	 * @return Image-Layer with the lines & arrows painted on it
+	 */
+	private Image generateArrowLayer(GraphicsConfiguration dc) {
+		// Useful geometric calculations
+		int
+			w = getWidth() - 1,
+			h = getHeight() - 1,
+			sz = Math.min(w,  h),
+			x = (w - sz) / 2,
+			y = (h - sz) / 2,
+			arrowX, arrowY;
+
+		// Initialize Layer-Image
+		BufferedImage img = dc.createCompatibleImage(w, h, Transparency.TRANSLUCENT);
+		Graphics2D gfx = img.createGraphics();
+		gfx.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+		// Draw Lines
+		gfx.setColor(Color.BLACK);
+		gfx.drawLine(x, y+sz-1, x+sz, y+sz-1);
+		gfx.drawLine(x+sz, y, x+sz, y+sz);
+
+		// save Transformation
+		AffineTransform oldTransform = gfx.getTransform();
+
+		// Roll-Arrow on the horizontal Arrow
+		arrowX = (int) (x - 5 + ((1 - roll / 360) * sz));
+		arrowY = y+sz-10;
+
+		// Translate into place and paint
+		gfx.translate(arrowX, arrowY);
+		gfx.fillPolygon(triangle);
+
+		// restore Transformation
+		gfx.setTransform(oldTransform);
+
+		// Pitch-Arrow on the vertical Arrow
+		arrowX = x + sz - 10;
+		arrowY = (int) (y + 5 + ((pitch / 360) * sz));
+
+		// Translate & Rotate into place and paint
+		gfx.rotate(Math.toRadians(-90), arrowX, arrowY);
+		gfx.translate(arrowX, arrowY);
+		gfx.fillPolygon(triangle);
+
+		// return the Layer-Image
+		gfx.dispose();
+		return img;
+	}
+
+
+
 
 	/**
 	 * get the currently displayed roll-value in degrees (0-360°)
@@ -253,6 +306,8 @@ public class HorizonPanel extends JPanel {
 		this.pitch = normalize(pitch);
 		this.repaint();
 	}
+
+
 
 	/**
 	 * normalize angle value to 0-360°
